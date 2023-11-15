7e3bc34db8af52f1f8ed6b17c7e8819f7d9e5c1e
Amdiatou TIMERA <amdiatoutimera@hotmail.fr>
2023-11-15 15:38:26 +0100

    Sonar 3

--- src/main/java/com/atimera/App.java
+++ src/main/java/com/atimera/App.java
@@ -1,5 +1,7 @@
 package com.atimera;
 
+import java.util.logging.Logger;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Logger.getGlobal().info("Hello world");
     }
 }
